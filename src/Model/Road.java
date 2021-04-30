103a29c2f6cf76a18f7f9fd122b15b51e69a4cb0
WarkM <37866728+WarkM@users.noreply.github.com>
2021-04-30 15:22:07 +0200

    Adapt the number of point depending on the road size

--- src/Model/Road.java
+++ src/Model/Road.java
@@ -34,7 +34,6 @@ public class Road {
         // We need to know how many point there is between two city, the longer the road is -> more point
         double distance = Math.sqrt(Math.pow(pointB.getX() - pointA.getX(),2) + Math.pow(pointB.getY() - pointA.getY(),2));
         int pointNum = (int)(distance/10) ;
-        System.out.println("point" +  pointNum);
 
         coordsList.clear();
         coordsList.add(pointA);
